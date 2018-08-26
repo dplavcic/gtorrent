814f4f6cdb2cda4c97784d99353d031754ab9070
dplavcic <dalibor.plavcic@gmail.com>
2018-08-26 13:42:49 +0200

    WIP - added support for []byte field

--- bencode/decode.go
+++ bencode/decode.go
@@ -8,34 +8,16 @@ import (
 	"reflect"
 	"strconv"
 )
-
+q
 type Decode struct {
 	buf *bytes.Buffer
-	// flags used to pare 'info' dict as []byte
-	start    int
-	stop     int
-	original int
-	setStop  bool
 }
 
 func Unmarshal(data []byte, v interface{}) error {
 	buf := bytes.NewBuffer(data)
-	d := Decode{buf: buf, original: buf.Len(), setStop: true}
+	d := Decode{buf: buf}
 	r := d.unmarshal(v)
-	// todo(dplavcic) handle this better!!!
-	d.handleInfoDict(v)
 	return r
-}
-
-func (d *Decode) handleInfoDict(v interface{}) {
-	fv, e := d.fieldName(reflect.ValueOf(v).Elem(), "InfoByte")
-	if e != nil {
-		log.Panicf("Could not find field with key: %v, %v", "InfoByte", e)
-	}
-	d.buf.Reset()
-	b := d.buf.Bytes()
-	b1 := b[d.start:d.stop]
-	fv.Set(reflect.ValueOf(b1))
 }
 
 func (d *Decode) unmarshal(v interface{}) (err error) {
@@ -75,6 +57,13 @@ func (d *Decode) readNext(v reflect.Value) {
 }
 
 func (d *Decode) parseDict(v reflect.Value) {
+
+	var i []uint8
+	if v.Kind() == reflect.Slice && v.Type() == reflect.TypeOf(i) {
+		d.buf.UnreadByte()
+		d.parseByteArray(v)
+	}
+
 	for {
 		b, err := d.buf.ReadByte()
 		if err != nil {
@@ -82,21 +71,12 @@ func (d *Decode) parseDict(v reflect.Value) {
 		}
 
 		if b == 'e' { //end of dict
-			if d.setStop {
-				d.stop = d.original - d.buf.Len()
-				d.setStop = false
-			}
-
 			return
 		}
 
 		//not end of dict
 		d.buf.UnreadByte()
-
 		key := d.readKey()
-		if key == "info" {
-			d.start = d.original - d.buf.Len()
-		}
 
 		var fv reflect.Value
 		if v.Kind() == reflect.Struct {
@@ -279,4 +259,102 @@ func (d *Decode) skipList(v reflect.Value) {
 		d.buf.UnreadByte()
 		d.skipValue(v)
 	}
+}
+
+func (d *Decode) parseByteArray(v reflect.Value) {
+	d.readNextByte(v)
+}
+
+func (d *Decode) readNextByte(v reflect.Value) {
+	b, err := d.buf.ReadByte()
+	if err != nil {
+		fmt.Errorf("%v", err)
+	}
+	switch b {
+	case 'd':
+		d.buf.UnreadByte()
+		d.parseDictByte(v)
+	case 'i':
+		d.buf.UnreadByte()
+		d.parseIntByte(v)
+	default:
+		d.buf.UnreadByte()
+		d.parseStringByte(v)
+	}
+}
+
+func (d *Decode) parseDictByte(v reflect.Value) {
+	dv, e := d.buf.ReadByte() // d
+	for i := 0; ; i++ {       //add key
+		if e != nil {
+			fmt.Errorf("%v", e)
+		}
+		sl, e := d.buf.ReadBytes(byte(':')) // 4:
+		if e != nil {
+			fmt.Errorf("%v", e)
+		}
+		slv := sl[:len(sl)-1]                            // 4
+		sliv, e := strconv.ParseInt(string(slv), 10, 64) // 4 as int64
+		if e != nil {
+			fmt.Errorf("%v", e)
+		}
+		buffer := make([]byte, sliv, sliv) // []byte{info}
+		d.buf.Read(buffer)
+
+		var r []byte
+		if i == 0 {
+			r = make([]byte, 0, 1+len(sl)+len(buffer))
+			r = append(r, dv)
+			r = append(r, sl...)
+			r = append(r, buffer...)
+		} else {
+			r = make([]byte, 0, len(sl)+len(buffer))
+			r = append(r, sl...)
+			r = append(r, buffer...)
+		}
+
+		rv := reflect.AppendSlice(v, reflect.ValueOf(r))
+		v.Set(rv)
+		d.readNextByte(v)
+		b, _ := d.buf.ReadByte()
+		if b == 'e' {
+			rv = reflect.Append(v, reflect.ValueOf(b))
+			v.Set(rv)
+			return
+		}
+		d.buf.UnreadByte()
+	}
+}
+
+func (d *Decode) parseIntByte(v reflect.Value) {
+	i, _ := d.buf.ReadByte()            // i
+	iv, _ := d.buf.ReadBytes(byte('e')) // 3e
+
+	r := make([]byte, 0, 1+len(iv))
+	r = append(r, i)
+	r = append(r, iv...)
+
+	rv := reflect.AppendSlice(v, reflect.ValueOf(r))
+	v.Set(rv)
+}
+
+func (d *Decode) parseStringByte(v reflect.Value) {
+	sl, e := d.buf.ReadBytes(byte(':')) // 4:
+	if e != nil {
+		fmt.Errorf("%v", e)
+	}
+	slv := sl[:len(sl)-1]                            // 4
+	sliv, e := strconv.ParseInt(string(slv), 10, 64) // 4 as int64
+	if e != nil {
+		fmt.Errorf("%v", e)
+	}
+	buffer := make([]byte, sliv, sliv) // []byte{info}
+	d.buf.Read(buffer)
+
+	r := make([]byte, 0, 1+len(sl)+len(buffer))
+	r = append(r, sl...)
+	r = append(r, buffer...)
+
+	rv := reflect.AppendSlice(v, reflect.ValueOf(r))
+	v.Set(rv)
 }
--- bencode/decode_test.go
+++ bencode/decode_test.go
@@ -18,8 +18,7 @@ type Torrent struct {
 	AnnounceList []interface{} `bencode:"announce-list"`
 	Comment      string        `bencode:"comment"`
 	CreationDate int           `bencode:"creation date"`
-	Info         Info          `bencode:"info"`
-	InfoByte     []byte
+	Info         []byte        `bencode:"info"`
 	HTTPSeeds    []interface{} `bencode:"http seeds"`
 	Checksum     string        `bencode:"checksum"`
 	CreatedBy    string        `bencode:"created by"`
@@ -44,6 +43,17 @@ type File struct {
 	Name   string   `bencode:"name"`
 }
 
+func TestUnmarshalByteField(t *testing.T) {
+	f, e := ioutil.ReadFile("../ubuntu_server_info_dict.torrent")
+	if e != nil {
+		t.Error(e)
+	}
+	var infoDict Info
+	Unmarshal(f, &infoDict)
+
+	t.Errorf("info dict: %#v\n", infoDict)
+}
+
 func TestUnmarshal(t *testing.T) {
 	var tr Torrent
 	f, e := ioutil.ReadFile("../ubuntu_server.torrent")
@@ -63,7 +73,7 @@ func TestInfoDictHash(t *testing.T) {
 	}
 
 	Unmarshal(f, &tr)
-	h := calculateSha1(tr.InfoByte)
+	h := calculateSha1(tr.Info)
 
 	got := url.PathEscape(string(h))
 	want := strings.ToUpper("%965%ca%bd%20%1a%92%fd%a5%d6%c7%0d5%cb%fbp~%29%05%89")
